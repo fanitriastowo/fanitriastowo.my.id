bd58462a7e590e7a2f82790cb62957112d469329
Fani Triastowo <fani.triastowo@gmail.com>
2024-12-10 16:46:21 +0700

    chore: fixed spelling

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <h2 className="text-2lg">About</h2>
       <p>
         I'm a Fullstack Developer since 2019 with wide range of programming language experience.
-        Having worked for companies such as <span>Zodiac Solutions Ltd</span>, <span>Qytatrans Group</span>,
+        Worked for companies such as <span>Zodiac Solutions Ltd</span>, <span>Qytatrans Group</span>,
         and <span>Universitas Muhammadiyah Purwokerto</span>.
       </p>
     </div>
--- src/components/WorkExperience/index.tsx
+++ src/components/WorkExperience/index.tsx
@@ -9,12 +9,12 @@ export default function WorkExperience() {
         <div className="job">
           <h3>Zodiac Solutions Ltd</h3>
           <div className="experience-badge">Started 2020 - 2024 (4 years)</div>
-          <p>I was hired by Zodiac Solutions Ltd which is based on Singapore as a Frontend developer for the first 2 years, and switch to Backend developer in the last 2 years at the company. I was working remotely to handle multiple virtual tour exhibitions and other online events. Most of the technologies we were using included: NodeJS (Typescript), NextJS, NestJS, Supabase, Firebase, KRPano, and automatic1111 and comfyui integration.</p>
+          <p>I was hired by Zodiac Solutions Ltd which is based on Singapore as a Frontend developer for the first 2 years, and switch to Backend developer in the last 2 years at the company. I was working remotely to handle multiple virtual tour exhibitions and other online events. Most of the technologies we were using include: NodeJS (Typescript), NextJS, NestJS, Supabase, Firebase, KRPano, and automatic1111 and comfyui integration.</p>
         </div>
         <div className="job">
           <h3>Qytatrans Group</h3>
           <div className="experience-badge">Started 2019 - 2020 (1 year)</div>
-          <p>I was hired by Qytatrans Group as Backend developer for 1 year. Most of the work I've done were internal system that consist of reservation system, finance system, and human resource system. The stack I used where: Laravel, Bootstrap and JQuery.</p>
+          <p>I was hired by Qytatrans Group as Backend developer for a year. Most of the work were internal systems that consist of reservation system, finance system, and human resource system. The stacks I used where: Laravel, Bootstrap and JQuery.</p>
         </div>
         <div className="job">
           <h3>PT. Swadharma Sarana Informatika</h3>
@@ -24,7 +24,7 @@ export default function WorkExperience() {
         <div className="job">
           <h3>Universitas Muhammadiyah Purwokerto</h3>
           <div className="experience-badge">Started 2015 - 2018 (3 year)</div>
-          <p>This is the first career I have after graduation. I was hired at my own university 1 month before graduation. I worked as IT support and switch to programmer at the time to handle multiple internal system. Mostly academic system, new enrollment student system and human resource system. The stack consist of PHP, MySQL, Bootstrap, and JQuery</p>
+          <p>This is the first career I have after graduation. I was hired by my university 1 month prior of graduation. I worked as IT support and switch to programmer to handle multiple internal systems. Mostly academic system, enrollment system and human resource. The stacks consist of PHP, MySQL, Bootstrap, and JQuery</p>
         </div>
       </div>
     </div>
