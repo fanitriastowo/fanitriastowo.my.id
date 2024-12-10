c3432c01c0e3076b4ab4fda073cd7965ad938474
Fani Triastowo <fani.triastowo@gmail.com>
2024-12-10 13:45:01 +0700

    infra: added about and work experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import About from "./components/About"
 import Header from "./components/Header"
+import WorkExperience from "./components/WorkExperience"
 
 function App() {
   return (
     <>
       <Header />
+      <About />
+      <WorkExperience />
     </>
   )
 }
---
+++ src/components/About/index.tsx
@@ -0,0 +1,14 @@
+import "./style.css"
+
+export default function About() {
+  return (
+    <div className="about">
+      <h2 className="text-2lg">About</h2>
+      <p>
+        I'm a Fullstack Developer since 2019 with wide range of programming language experience.
+        Having worked for companies such as <span>Zodiac Solutions Ltd</span>, <span>Qytatrans Group</span>,
+        and <span>Universitas Muhammadiyah Purwokerto</span>.
+      </p>
+    </div>
+  )
+}
